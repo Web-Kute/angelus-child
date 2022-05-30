a4c46d6dedd0dec6325c0c6d21f3c71d0a48fa4a
Ikigai <alvardsandz@gmail.com>
2022-05-30 17:50:34 +0200

    add click outside partners panel

--- template-parts/partners.php
+++ template-parts/partners.php
@@ -1,7 +1,7 @@
 <!-- Partners -->
 	<div id="box-partners">
-	    <div id="partners" class="container-fluid" tabindex="-1" aria-hidden="true">
-	        <span class="close-cross">
+	    <div id="partners" class="container-fluid" tabindex="0" aria-hidden="true">
+	        <span id="close-cross">
 	        	<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="times-circle" class="svg-inline--fa fa-times-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z"></path>
 	        	</svg>
 	        </span>
@@ -80,6 +80,6 @@
               </div>
           </div>
 	    </div>
-	    <div class="btn-open close-partners" role="button">Nos partenaires</div>
+	    <div class="btn-open close-partners" id="close-bottom" role="button">Nos partenaires</div>
 	</div>
 <!-- End Partners -->
